34c850f5c68ce95caf5904d2bc042ad3cd6dd74f
rkdaredevil <rupeshkumarthakur11@gmail.com>
2018-10-16 17:18:33 +0530

    user upload and profile create

--- src/api/controllers/upload.controller.js
+++ src/api/controllers/upload.controller.js
@@ -1,7 +1,26 @@
 const AWS = require('aws-sdk');
 const User = require('../models/user.model');
-exports.uploadFiles = function(req, res, next) {
+const _ = require('lodash');
+const RefreshToken = require('../models/refreshToken.model');
+const moment = require('moment-timezone');
+const {
+	jwtExpirationInterval
+} = require('../../config/vars');
 
+function generateTokenResponse(user, accessToken) {
+	const tokenType = 'Bearer';
+	const refreshToken = RefreshToken.generate(user).token;
+	const expiresIn = moment().add(jwtExpirationInterval, 'days');
+	return {
+		tokenType,
+		accessToken,
+		refreshToken,
+		expiresIn,
+	};
+}
+
+exports.uploadFiles = function(req, res, next) {
+	let _id = req.body.id;
 	let file = req.files;
 	let s3bucket = new AWS.S3({
 		accessKeyId: '',
@@ -22,38 +41,40 @@ exports.uploadFiles = function(req, res, next) {
 				ACL: 'public-read'
 			};
 			s3bucket.upload(params, function(err, data) {
-				if (err) {
-					res.json({
-						"error": true,
-						"Message": err
-					});
-				} else {
-					ResponseData.push(data);
 
-					User.findOne({
-						$or: [{
-							_id: req.body.id
-						}, {
-							phone: req.body.phone
-						}]
-
-					}).exec(function(err, user) {
+				ResponseData.push(data);
+				if (_id) {
+					User.findById(_id, function(err, user) {
 						for (var item of ResponseData) {
 							user.images.push(item.Location);
-							user.save()
-								.then(savedUser => res.status(200).json({
-									images: savedUser.images
-								}))
-								.catch(e => next(User.checkDuplicateEmail(e)));
-
+							user.save(function(err, data) {
+								if (err) {
+									res.status(422).send({
+										message: 'something went wrong'
+									})
+								} else {
+									res.status(200).send(data);
+								}
+							})
 						}
+					})
+				} else {
 
-					});
+					var body = _.pick(req.body, ['name', 'phone', 'password', 'gender']);
+					var user = new User(req.body);
+					for (var item of ResponseData) {
+						user.images.push(item.Location);
+						user.save()
+							.then(savedUser => res.status(200).json({
+								token,
+								user: savedUser
+							}))
+							.catch(e => res.status(400).send(e));
 
-
-
+					}
 				}
+
 			});
 		});
 	});
-}
+};
